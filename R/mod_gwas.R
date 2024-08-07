ffcfc72dc8f17efa71b23348f150615ccbff895f
Alexander Sandercock <39815775+alex-sandercock@users.noreply.github.com>
2024-08-07 11:49:07 -0400

    Fixed bug with GWAS executables location

--- R/mod_gwas.R
+++ R/mod_gwas.R
@@ -92,6 +92,8 @@ mod_gwas_ui <- function(id){
 #' @importFrom DT renderDT
 #' @importFrom vcfR read.vcfR
 #' @import stats
+#' @importFrom matrixcalc is.positive.definite
+#' @importFrom Matrix nearPD
 #' @noRd
 mod_gwas_server <- function(id){
   moduleServer( id, function(input, output, session){
@@ -263,7 +265,7 @@ mod_gwas_server <- function(id){
       #Status
       updateProgressBar(session = session, id = "pb_gwas", value = 20, title = "Formatting Complete: Now Calculating BIC")
 
-      source("FUN/MyFun_BIC_Meng.R") #change directory in your case
+      source("R/MyFun_BIC_Meng.R") #change directory in your case
 
       PC<-as.matrix(PCs)
       K=as.matrix(Kin)
@@ -374,7 +376,7 @@ mod_gwas_server <- function(id){
         #get qqplot
         data_qq <- cbind.data.frame(SNP=data.loco.scan@map$Marker,Chr=data.loco.scan@map$Chrom, Pos=data.loco.scan@map$Position,10^(-data.loco.scan@scores[[colnames(data@pheno[i])]]))
 
-        source("FUN/CMplot.r") #Obtained the CMplot code from GitHub and made edits to allow inline plotting for shiny app
+        source("R/CMplot.r") #Obtained the CMplot code from GitHub and made edits to allow inline plotting for shiny app
 
         #Save qq_plot info
         gwas_vars$qq_plots <- data_qq
@@ -479,7 +481,7 @@ mod_gwas_server <- function(id){
         } else if (input$gwas_figures == "QQ Plot") {
           req(gwas_vars$qq_plots)
 
-          source("FUN/CMplot.r") #Obtained the CMplot code from GitHub and made edits to allow inline plotting for shiny app
+          source("R/CMplot.r") #Obtained the CMplot code from GitHub and made edits to allow inline plotting for shiny app
 
           #Plot
           CMplot_shiny(gwas_vars$qq_plots,plot.type="q",col=c(1:8),
